f667f7eca7ebe3a23d335d0c4f9b35471a364e26
hjacobs <henning@jacobs1.de>
2016-01-28 22:30:16 +0100

    attempt to build bootstrap Docker image

--- install.sh
+++ install.sh
@@ -5,56 +5,26 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
-export DEBIAN_FRONTEND=noninteractive
+# install Docker if necessary
+docker=$(which docker)
+if [ ! -x $docker ]; then
+    apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-# install Docker
-apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+    codename=$(lsb_release -cs)
+    echo -e "deb https://apt.dockerproject.org/repo ubuntu-$codename main" > /etc/apt/sources.list.d/docker.list
 
-codename=$(lsb_release -cs)
-echo -e "deb https://apt.dockerproject.org/repo ubuntu-$codename main" > /etc/apt/sources.list.d/docker.list
-
-apt-get -y update
-apt-get -y install docker-engine
-
-apt-get install -y postgresql-client git redis-tools python3-pip makepasswd netcat-openbsd
-
-pip3 install --upgrade pip
-pip3 install --upgrade zmon-cli
-
-mkdir -p zmon-controller
-git clone https://github.com/zalando/zmon-controller.git zmon-controller || echo 'zmon-controller seems to be cloned already'
-
-mkdir -p zmon-eventlog-service
-git clone https://github.com/zalando/zmon-eventlog-service.git zmon-eventlog-service || echo 'zmon-eventlog-service seems to be cloned already'
-
-# set up PostgreSQL
-export PGHOST=localhost
-export PGUSER=postgres
-export PGPASSWORD=$(makepasswd --chars 32)
-export PGDATABASE=demo_zmon_db
-
-echo "localhost:5432:*:postgres:$PGPASSWORD" > ~/.pgpass
-chmod 600 ~/.pgpass
-
-container=$(docker ps | grep postgres)
-if [ -z "$container" ]; then
-    docker rm postgres
-    docker run --restart "on-failure:10" --name postgres --net host -e POSTGRES_PASSWORD=$PGPASSWORD -d registry.opensource.zalan.do/stups/postgres:9.4.5-1
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get -y update
+    apt-get -y install docker-engine
 fi
 
-until nc -w 5 -z localhost 5432; do
-    echo 'Waiting for Postgres port..'
-    sleep 3
-done
+docker network create --driver bridge zmon-demo
 
-# set up Redis
-container=$(docker ps | grep redis)
-if [ -z "$container" ]; then
-    docker rm redis
-    docker run --restart "on-failure:10" --name redis --net host -d registry.opensource.zalan.do/stups/redis:3.0.5
-fi
+docker rm -f zmon-demo-bootstrap
+docker run -it --name zmon-demo-bootstrap \
+    -v $(pwd):/workdir \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /usr/bin/docker:/usr/bin/docker \
+    --net zmon-demo \
+    zmon-demo-bootstrap
 
-until nc -w 5 -z localhost 6379; do
-    echo 'Waiting for Redis port..'
-    sleep 3
-done
